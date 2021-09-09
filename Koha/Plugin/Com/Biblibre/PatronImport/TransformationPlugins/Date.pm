b5468799d239732202ee3129047cd28d0591c349
Alex Arnaud <alex.arnaud@biblibre.com>
2021-09-09 15:10:24 +0200

    Add Date and Today plugins

---
+++ Koha/Plugin/Com/Biblibre/PatronImport/TransformationPlugins/Date.pm
@@ -0,0 +1,24 @@
+package Koha::Plugin::Com::Biblibre::PatronImport::TransformationPlugins::Date;
+
+use Modern::Perl;
+
+sub transform {
+    my ($value) = @_;
+
+    # Ymd
+    if ($value =~ /^(\d{4})(\d{2})(\d{2})$/ ) {
+	return "$1-$2-$3";
+    }
+
+    # Y/m/d
+    if ($value =~ /^(\d{4})\/(\d{2})\/(\d{2})$/ ) {
+	return "$1-$2-$3";
+    }
+
+    # d/m/Y
+    if ($value =~ /^(\d{2})\/(\d{2})\/(\d{4})$/ ) {
+	return "$3-$2-$1";
+    }
+}
+
+1;
