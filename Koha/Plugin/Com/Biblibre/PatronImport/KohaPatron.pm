b0b6ef48123bde92495affec4ccded4d416d5abf
Thibaud Guillot <thibaud.guillot@biblibre.com>
2023-10-03 15:56:51 +0200

    Fix mixed rules on xattr and fields
    To respect rules according to field and value mapping, add is_xattr function usage to create a conditionnal treatment

--- Koha/Plugin/Com/Biblibre/PatronImport/KohaPatron.pm
+++ Koha/Plugin/Com/Biblibre/PatronImport/KohaPatron.pm
@@ -745,22 +745,24 @@ sub _rule_match {
     }
 
     foreach my $field ( keys %{ $rule->{fields} } ) {
-        if ( is_set( $patron->{$field} ) && $patron->{$field} ne $rule->{fields}->{$field} ) {
-            return 0;
-        } elsif ( is_empty( $patron->{$field} ) && $rule->{fields}->{$field} ne '' ) {
-            return 0;
-        }
+        if( is_xattr($field) ) {
+            foreach my $xattr ( @{ $patron->{xattr} } ) {
+                my $code      = $xattr->{code};
+                my $attribute = $xattr->{attribute};
 
-        foreach my $xattr ( @{ $patron->{xattr} } ) {
-            my $code      = $xattr->{code};
-            my $attribute = $xattr->{attribute};
-
-            if ( $code eq $field ) {
-                if ( is_set($attribute) && $attribute ne $rule->{fields}->{$field} ) {
-                    return 0;
-                } elsif ( is_empty($attribute) && $rule->{fields}->{$field} ne '' ) {
-                    return 0;
+                if ( $code eq $field ) {
+                    if ( is_set($attribute) && $attribute ne $rule->{fields}->{$field} ) {
+                        return 0;
+                    } elsif ( is_empty($attribute) && $rule->{fields}->{$field} ne '' ) {
+                        return 0;
+                    }
                 }
+            }
+        } else {
+            if ( is_set( $patron->{$field} ) && $patron->{$field} ne $rule->{fields}->{$field} ) {
+                return 0;
+            } elsif ( is_empty( $patron->{$field} ) && $rule->{fields}->{$field} ne '' ) {
+                return 0;
             }
         }
     }
